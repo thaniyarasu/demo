d413e1211e4b6f0957995fc10da7421ed6fcbe7a
thani <thaniyarasu.k@valuelinked.com>
2013-02-15 17:42:38 +0530

    Post resources removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Demo::Application.routes.draw do
-  resources :posts
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
